dc027ae2453174313ad77707050f6f3be27a5221
Ankit Singh <ankitsinghrjt794@gmail.com>
2025-07-07 22:05:28 +0530

    Final Editing before ESD

--- src/Components/LazyYouTube.jsx
+++ src/Components/LazyYouTube.jsx
@@ -1,25 +1,30 @@
-// components/LazyYouTube.jsx
 import React from "react";
 import { useInView } from "react-intersection-observer";
 import YouTube from "react-youtube";
+import "./LazyYouTube.css"; // make sure this contains the styles
 
-const LazyYouTube = ({ videoId, opts }) => {
+const LazyYouTube = ({ videoId }) => {
   const { ref, inView } = useInView({
     triggerOnce: true,
     threshold: 0.1,
   });
 
   return (
-    <div ref={ref} style={{ minHeight: '390px', marginBottom: '2rem' }}>
+    <div className="video-wrapper" ref={ref}>
       {inView ? (
-        <YouTube videoId={videoId} opts={opts} />
+        <YouTube
+          videoId={videoId}
+          className="video-iframe"
+          opts={{
+            width: "100%",
+            height: "100%",
+            playerVars: {
+              autoplay: 0,
+            },
+          }}
+        />
       ) : (
-        <div style={{
-          width: opts.width,
-          height: opts.height,
-          background: '#000',
-          borderRadius: '12px'
-        }} />
+        <div className="video-placeholder" />
       )}
     </div>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -23,32 +23,32 @@ export default function Home() {
         <div className="home">
             <Navbar />
             <div className="home-container">
-            <div className="home-content">
-                <SplitText
-                    text="EmpowerHub"
-                    className="text-10xl font-semibold text-center font-tangerine"
-                    delay={100}
-                    duration={0.6}
-                    ease="power3.out"
-                    splitType="chars"
-                    from={{ opacity: 0, y: 40 }}
-                    to={{ opacity: 1, y: 0 }}
-                    threshold={0.1}
-                    rootMargin="-100px"
-                    textAlign="center"
-                    onLetterAnimationComplete={handleAnimationComplete}
-                />
-                <br/>Empowering You to Build, Learn, and Lead.
-                <div className="home-buttons">
-                <StarBorder
-                    as="button"
-                    className="custom-class"
-                    color="magenta"
-                    speed="5s"
-                >
-                    Get Started
-                </StarBorder>
-                </div>
+                <div className="home-content">
+                    <SplitText
+                        text="EmpowerHub"
+                        className="text-10xl font-semibold text-center font-tangerine"
+                        delay={100}
+                        duration={0.6}
+                        ease="power3.out"
+                        splitType="chars"
+                        from={{ opacity: 0, y: 40 }}
+                        to={{ opacity: 1, y: 0 }}
+                        threshold={0.1}
+                        rootMargin="-100px"
+                        textAlign="center"
+                        onLetterAnimationComplete={handleAnimationComplete}
+                    />
+                    <br />Empowering You to Build, Learn, and Lead.
+                    <div className="home-buttons">
+                        <StarBorder
+                            as="button"
+                            className="custom-class"
+                            color="magenta"
+                            speed="5s"
+                        >
+                            Get Started
+                        </StarBorder>
+                    </div>
                 </div>
                 <img src="https://res.cloudinary.com/divulwxho/image/upload/v1751714762/img1_pxoc9g.svg" alt="Placeholder" className="image-1" />
             </div>
@@ -68,22 +68,24 @@ export default function Home() {
                     textAlign="center"
                 />
             </div>
-            <div className="yt-videos">
-            <LazyYouTube videoId="J11Qme3vAio" opts={opts} />
-            <LazyYouTube videoId="T9ti0_I04Xs" opts={opts} />
-            <LazyYouTube videoId="2qH7ausehwE" opts={opts} />
-            <LazyYouTube videoId="Z0fZ_GIT7vo" opts={opts} />
-            <LazyYouTube videoId="O0VFTXkXcAE" opts={opts} />
-            <LazyYouTube videoId="YG1sW00jwLY" opts={opts} />
-            <LazyYouTube videoId="F6Nh1l-T1qY" opts={opts} />
-            <LazyYouTube videoId="JJjBPBWaeYU" opts={opts} />
-            <LazyYouTube videoId="THV25A4kIr0" opts={opts} />
-            <LazyYouTube videoId="VTLCoHnyACE" opts={opts} />
-            <LazyYouTube videoId="KnBwHYAnbEw" opts={opts} />
-            <LazyYouTube videoId="qrbf9DtR3_c" opts={opts} />
-            <LazyYouTube videoId="AEKBlW81SAc" opts={opts} />
-            <LazyYouTube videoId="2MCmnr2L50o" opts={opts} />
-            <LazyYouTube videoId="LfaMVlDaQ24" opts={opts} />
+            <div className="home-content-3">
+                <div className="yt-videos">
+                    <LazyYouTube videoId="J11Qme3vAio" opts={opts} />
+                    <LazyYouTube videoId="T9ti0_I04Xs" opts={opts} />
+                    <LazyYouTube videoId="2qH7ausehwE" opts={opts} />
+                    <LazyYouTube videoId="Z0fZ_GIT7vo" opts={opts} />
+                    <LazyYouTube videoId="O0VFTXkXcAE" opts={opts} />
+                    <LazyYouTube videoId="YG1sW00jwLY" opts={opts} />
+                    <LazyYouTube videoId="F6Nh1l-T1qY" opts={opts} />
+                    <LazyYouTube videoId="JJjBPBWaeYU" opts={opts} />
+                    <LazyYouTube videoId="THV25A4kIr0" opts={opts} />
+                    <LazyYouTube videoId="VTLCoHnyACE" opts={opts} />
+                    <LazyYouTube videoId="KnBwHYAnbEw" opts={opts} />
+                    <LazyYouTube videoId="qrbf9DtR3_c" opts={opts} />
+                    <LazyYouTube videoId="AEKBlW81SAc" opts={opts} />
+                    <LazyYouTube videoId="2MCmnr2L50o" opts={opts} />
+                    <LazyYouTube videoId="LfaMVlDaQ24" opts={opts} />
+                </div>
             </div>
         </div>
     );
